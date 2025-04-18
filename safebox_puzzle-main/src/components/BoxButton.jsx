f2de04ae05dbdb33499556511fb312abf07f5c6c
Miguelkoro <miguelkoro@gmail.com>
2025-04-18 17:57:48 +0200

    Añadidos los botones, video por defecto, comprobacion del canal en plan guarro y su interfaz en la television. Quedaria añadir interfaz del volumen

--- safebox_puzzle-main/src/components/BoxButton.jsx
+++ safebox_puzzle-main/src/components/BoxButton.jsx
@@ -2,8 +2,11 @@ import React from 'react';
 
 const BoxButton = (props) => {
   return (
-    <div className={"boxButton boxButton" + props.position} onClick={() => props.onClick(props.value)} style={{ width: props.boxWidth * 0.06, height: props.boxHeight * 0.1 }}>
+    <div className={"boxButton boxButton" + props.position} onClick={() => props.onClick(props.value)} style={{ width: props.boxWidth * 0.04, height: "auto", // Ajusta automáticamente la altura al contenido
+      display: "inline-block", // Asegura que el tamaño se ajuste al contenido
+      overflow: "hidden", }}>
       <li>
+
         <p>{props.value}</p>
       </li>
     </div>
--- safebox_puzzle-main/src/components/MainScreen.jsx
+++ safebox_puzzle-main/src/components/MainScreen.jsx
@@ -1,10 +1,13 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useRef, useEffect } from 'react';
 import './../assets/scss/main.scss';
-
+import BoxButton from './BoxButton';
 import VideoJS from './VideoJS'
+import videojs from 'video.js';
+//import videojs from 'video.js';
 import "video.js/dist/video-js.css";
 import "videojs-youtube";
 import televisionImage from "../assets/images/Television.png";
+import { use } from 'react';
 
 const MainScreen = (props) => {
   const [checking, setChecking] = useState(false);
@@ -13,20 +16,25 @@ const MainScreen = (props) => {
   const [boxHeight, setBoxHeight] = useState(0);
 
   //
-  const [rotationAngle, setRotationAngle] = useState(0); // Estado para la rotación
-  const [isMouseDown, setIsMouseDown] = useState(false); // Estado para saber si el mouse está presionado
-  const [startAngle, setStartAngle] = useState(0); // Ángulo inicial del ratón
-  const [initialRotation, setInitialRotation] = useState(0); // Ángulo inicial del lock
+  const [isYoutube, setIsYoutube] = useState(false); // Estado para alternar entre MP4 y YouTube
+  //const playerRef = React.useRef(null);
+  const playerRef = useRef(null); // Referencia al reproductor de Video.js (Removed duplicate declaration)
+
+
 
   const [isReseting, setIsReseting] = useState(false); // Estado para saber si se está reiniciando el lock
   const [tries, setTries] = useState(0); // Contador de intentos
-  const [rotationDirection, setRotationDirection] = useState(""); // Dirección de rotación
 
   const [solutionArray, setSolutionArray] = useState([]); // Array para guardar la solución
+  const [solution, setSolution] = useState(""); // Variable para guardar la solución
+  const [timer, setTimer] = useState(null); // Temporizador para los 5 segundos
+  const [showCursor, setShowCursor] = useState(false); // Controla si se muestra el guion bajo
+  const [showSolution, setShowSolution] = useState(false); // Controla si se muestra el <p>
   
   const TEST_LOCAL = false; // Cambiar a true para usar la solución local
   const SOLUTION_LOCAL = [25,-55,50, -15, 30]; //La solucion que queremos, 25 derecha, 55 izq, 50 derecha...
   const PASSWORD_API = 12345; //Contraseña de la sala del escape room 
+  const MIN_LENGHT = 4; //Longitud minima de la contraseña
   // //Tiene que ser de 5 digitos o cambiarlo en el archivo config
 
 
@@ -65,10 +73,7 @@ const MainScreen = (props) => {
   const  reset = () =>{
     //console.log("Solution: ", solutionArray);
     setIsReseting(true);
-    setRotationAngle(0);
-    setSolutionArray([]);
-    setRotationDirection("");
-    setStartAngle(0);
+
     setTries(0);
     setTimeout(() => {      
       setIsReseting(false);
@@ -104,97 +109,151 @@ const MainScreen = (props) => {
       tries >= props.config.passwordLength ? checkApiSolution() : setTries((tries) => tries + 1);
   }, [solutionArray]);
 
-  const handleMouseDown = (event) => {
-    if (checking) return ;
-    setIsMouseDown(true); // Indica que el mouse está presionado    
-    let rounded = calculateAngle(event); // Calcula el ángulo inicial
-    setStartAngle(rounded);     // Guarda el ángulo inicial y el ángulo actual del lock
-    setInitialRotation(rotationAngle); // Guarda el ángulo actual del lock    
-  };
 
-  const calculateAngle = (event) => {
-    const lockElement = document.getElementById("lock");
-    const rect = lockElement.getBoundingClientRect();  
-    // Calcula el centro del div
-    const centerX = rect.left + rect.width / 2;
-    const centerY = rect.top + rect.height / 2;  
-    // Calcula el ángulo inicial en radianes y lo convierte a grados
-    const radians = Math.atan2(event.clientY - centerY, event.clientX - centerX);
-    let angle = radians * (180 / Math.PI);  
-    // Normaliza el ángulo para que esté entre 0° y 360°
-    if (angle < 0) {
-      angle += 360;}
-    return Math.round(angle / 6) * 6;
-  }
-  
-  const handleMouseUp = () => {
-    if (checking) return ;
-    setIsMouseDown(false); // Indica que el mouse ya no está presionado
-    //reset(); // Reinicia la rotación //Poniendolo aqui, hace efecto de teelfono de dial
-    //Para poder poner -55 si va contrarreloj o 30 si va a favor
-    setSolutionArray((sol) => [...sol, (rotationDirection === "clockwise" ? rotationAngle/6 : -rotationAngle/6)]);
-    setRotationDirection(''); //Reinicia la direccion de rotacion
-  };
-
-  function getRotationDirection(prev, curr) {
-    const diff = (curr - prev + 60) % 60;
-    if (diff === 0) return '';
-    return diff < 30 ? 'clockwise' : 'counter-clockwise';
-  }
-
-  const normalizeAngleDifference = (angle) => {
-    return ((angle + 180) % 360) - 180;
-  };
-
-  const normalizeAngle = (angle) => {
-    return ((angle % 360) + 360) % 360; // Asegura que el ángulo esté entre 0 y 360
-  };
-
-  const handleMouseMove = (event) => {
-    if (!isMouseDown || checking) return ; // Solo ejecuta si el mouse está presionado    
-    let audio  = document.getElementById("audio_wheel");
-    let rounded = calculateAngle(event); // Calcula el ángulo 
-   // Calcula la diferencia de ángulos de forma cíclica
-    const angleDifference = normalizeAngleDifference(rounded - startAngle);
-   // Calcula la rotación acumulada y normalízala
-    const newRotation = normalizeAngle(initialRotation + angleDifference);
-    const rotationDir = getRotationDirection(rotationAngle/6, newRotation/6);
-    //Si se intenta girar en sentido contrario a la rotacion actual, no se hace nada
-    if(rotationDirection === ''){
-      setRotationDirection(rotationDir);
-    }else if(rotationDirection !== rotationDir){
-      return;}
-    if(rotationAngle === newRotation)return; // No actualiza si el ángulo no ha cambiado
-    setRotationAngle(newRotation);     // Actualiza el ángulo de rotación
-    audio.play();
-  };
   //  -----------------
+  // Actualiza la fuente del reproductor cuando cambia `isYoutube`
+  useEffect(() => {
+  // Actualiza las opciones del reproductor según el estado `isYoutube`
+  const newOptions = isYoutube ? youtubeVideoOptions : mp4VideoOptions;
+  setPlayerOptions(newOptions); // Guarda las opciones en el estado `playerOptions`
 
-  const playerRef = React.useRef(null);
+  if (playerRef.current) {
+    try{
+    playerRef.current.pause(); // Pausa el video actual
+    playerRef.current.src(newOptions.sources); // Cambia la fuente del reproductor
+    playerRef.current.load(); // Carga el nuevo video
+    playerRef.current.play(); // Reproduce el nuevo video
+    }catch(e){
+      console.error("Error al cambiar la fuente del reproductor:", e);
+    }
+  }
+  }, [isYoutube]);
 
-  const videoJsOptions = {
+ 
+  const onClickButton = (value) => {
+    //console.log("Button clicked: ", value);
+    if(checking) return; // Si ya se está comprobando, no hace nada
+    setSolution(prev => prev + value); // Agrega el valor del botón a la solución
+    const shortBeep = document.getElementById("audio_beep");
+    shortBeep.pause();
+    shortBeep.currentTime = 0;
+    shortBeep.play();
+   
+
+    // Activa el cursor y reinicia el temporizador de 5 segundos
+    setShowCursor(true);
+    setShowSolution(true); // Muestra el <p> con la solución
+    if (timer) {
+      clearTimeout(timer); // Limpia el temporizador anterior
+    }
+    const newTimer = setTimeout(() => {
+      //checkSolution(); // Comprueba la solución después de 5 segundos
+      
+      handleTimerExpire(); // Maneja la expiración del temporizador
+      //console.log("checking solution...", solution);
+    }, 5000);
+    setTimer(newTimer);
+  
+  }
+
+  // Función para manejar la expiración del temporizador
+  const handleTimerExpire = () => {
+    setChecking(true); // Activa el estado de checking
+    setShowCursor(false); // Desactiva el cursor
+    //console.log("Checking solution...", solution);
+    setTimeout(() => {
+      setShowSolution(false); // Oculta el <p> después de 3 segundos
+      setSolution(""); // Reinicia la solución
+      setChecking(false); // Reinicia el estado de checking
+      
+    }, 3000); // Espera 3 segundos antes de ocultar el <p>
+    
+  };
+
+  useEffect(() => {
+    if (solution.length > MIN_LENGHT) {
+      console.log("Checking solution...", solution);
+      Number(solution)===PASSWORD_API && playVideo()
+    }
+    
+  }, [checking]); // Se ejecuta cada vez que cambia la solución*/
+
+
+  // Opciones para el video MP4
+  const mp4VideoOptions = {
     autoplay: true,
-    controls: true,
+    controls: false,
     responsive: true,
     fluid: true,
-    techOrder: ['youtube'],
-    sources: [{
-      src: 'https://www.youtube.com/watch?v=iYYRH4apXDo',
-      type: 'video/youtube'
-    }]
+    muted: false,
+    loop: true,
+    techOrder: ["html5", "youtube"],
+    sources: [
+      {
+        src: "videos/OutputLoop.mov", // Reemplaza con la ruta de tu archivo MP4
+        type: "video/mp4",
+      },
+    ],
   };
+
+  // Opciones para el video de YouTube
+  const youtubeVideoOptions = {
+    autoplay: true,
+    controls: false,
+    responsive: true,
+    loop: true,
+    fluid: true,
+    techOrder: ["youtube"],
+    sources: [
+      {
+        src: "https://youtu.be/dQw4w9WgXcQ?si=ReWN7oDLo1kUD1zR&t=42",//"https://www.youtube.com/watch?v=iYYRH4apXDo",
+        type: "video/youtube",
+      },
+    ],
+    userActions: {
+      click: false
+    }
+  };
+
+  const [playerOptions, setPlayerOptions] = useState(mp4VideoOptions); // Estado para las opciones del reproductor
 
   const handlePlayerReady = (player) => {
     playerRef.current = player;
 
     // You can handle player events here, for example:
     player.on('waiting', () => {
-      videojs.log('player is waiting');
+      //videojs.log('player is waiting');
+      console.log("Waiting...", solution);
     });
 
     player.on('dispose', () => {
-      videojs.log('player will dispose');
+      //videojs.log('player will dispose');
     });
+  };
+
+  // Función para reproducir el video
+  const playVideo = () => {
+    setIsYoutube((prev) => !prev);
+  };
+
+  // Función para subir el volumen
+  const increaseVolume = () => {
+    if (playerRef.current) {
+      const currentVolume = playerRef.current.volume();
+      if (currentVolume < 1) {
+        playerRef.current.volume(Math.min(currentVolume + 0.1, 1)); // Incrementa el volumen en 0.1
+      }
+    }
+  };
+
+  // Función para bajar el volumen
+  const decreaseVolume = () => {
+    if (playerRef.current) {
+      const currentVolume = playerRef.current.volume();
+      if (currentVolume > 0) {
+        playerRef.current.volume(Math.max(currentVolume - 0.1, 0)); // Reduce el volumen en 0.1
+      }
+    }
   };
 
   return (
@@ -202,15 +261,45 @@ const MainScreen = (props) => {
         {props.show ? (         
          
           <div style={{width: boxWidth , height: boxHeight, position: "relative" }}>
-            <div style={{width: boxWidth *0.65, position:"absolute",  marginLeft: "7%", marginTop: "22%"}}>
-              <VideoJS options={videoJsOptions} onReady={handlePlayerReady}/>
+            <div style={{width: boxWidth *0.77, position:"absolute",  marginLeft: "0.5%", marginTop: "18.6%"}}>
+              <VideoJS  options={playerOptions}
+                onReady={handlePlayerReady}/>
+                
             </div>
             <img id="television" src={televisionImage} alt="Television" style={{width: boxWidth, height: boxHeight, position: "absolute", left: 0, top: 0}}/>
-         
+            {solution && (<p className={`channel ${showCursor ? "show-cursor" : ""}`}>{solution}</p>)}
+            {/*<button  onClick={playVideo} style={{position: "absolute", top: "10px",left: "10px", zIndex: 10,padding: "10px 20px",
+              backgroundColor: "#007BFF", color: "#fff", border: "none",borderRadius: "5px", cursor: "pointer", }}>
+            {isYoutube ? "Reproducir MP4" : "Reproducir YouTube"}
+          </button>*/}
+            {/* Fila de botones */}
+            <div id="row1" className="row" style={{ top: "42%"}}>
+              <BoxButton value={"1"} position={1} onClick={onClickButton} boxHeight={boxHeight} boxWidth={boxWidth} />
+              <BoxButton value={"2"} position={2} onClick={onClickButton} boxHeight={boxHeight} boxWidth={boxWidth} />
+              <BoxButton value={"3"} position={3} onClick={onClickButton} boxHeight={boxHeight} boxWidth={boxWidth} />
+            </div>
+            <div id="row2" className="row" style={{ top: "47%"}} >
+              <BoxButton value={"4"} position={4} onClick={onClickButton} boxHeight={boxHeight} boxWidth={boxWidth} />
+              <BoxButton value={"5"} position={5} onClick={onClickButton} boxHeight={boxHeight} boxWidth={boxWidth} />
+              <BoxButton value={"6"} position={6} onClick={onClickButton} boxHeight={boxHeight} boxWidth={boxWidth} />
+            </div>
+            <div id="row3" className="row" style={{ top: "52%" }}>
+              <BoxButton value={"7"} position={7} onClick={onClickButton} boxHeight={boxHeight} boxWidth={boxWidth} />
+              <BoxButton value={"8"} position={8} onClick={onClickButton} boxHeight={boxHeight} boxWidth={boxWidth} />
+              <BoxButton value={"9"} position={9} onClick={onClickButton} boxHeight={boxHeight} boxWidth={boxWidth} />
+            </div>
+            <div id="row4" className="row" style={{top: "57%"}}>
+              <BoxButton value={"0"} position={11} onClick={onClickButton} boxHeight={boxHeight} boxWidth={boxWidth} />
+            </div>
+            <div id="row4" className="row" style={{top: "63%"}}>
+              <BoxButton value={"-"} position={11} onClick={decreaseVolume} boxHeight={boxHeight} boxWidth={boxWidth} />
+              <svg width="20%" height="20%" viewBox="0 -1 22 22" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink" fill="#ffffff" stroke="#ffffff"> <g id="SVGRepo_bgCarrier" strokeWidth="0"></g>  <g id="SVGRepo_tracerCarrier" strokeLinecap="round" strokeLinejoin="round"></g> <g id="SVGRepo_iconCarrier"> <title>multimedia / 4 - multimedia, audio, music, sound, max, speaker, volume icon</title> <g id="Free-Icons" stroke="none" strokeWidth="1" fill="none" fillRule="evenodd" strokeLinecap="round" strokeLinejoin="round" > <g transform="translate(-968.000000, -304.000000)" id="Group" stroke="#000000" strokeWidth="2"> <g transform="translate(967.000000, 302.000000)" id="Shape"> <path d="M18.22291,4.24772391 C20.3461043,5.89188107 21.7500001,8.74918751 21.7500001,12 C21.7500001,15.2055503 20.384926,18.0284761 18.3111758,19.6828962"></path> <path d="M16.25,16.5 C17.434,15.6838509 18.25,13.984472 18.25,12.0055901 C18.25,10.0267081 17.434,8.32732919 16.25,7.5"></path> <path d="M4.254916,9 L6.24999966,9 L11.2499997,3 L13.2499997,3 L13.2499997,20.9958147 L11.2499997,20.9958147 L6.24999966,15 L4.254916,15 C3.1503465,15 2.254916,14.1045695 2.254916,13 L2.254916,11 C2.254916,9.8954305 3.1503465,9 4.254916,9 Z"></path></g> </g> </g></g></svg>
+              <BoxButton value={"+"} position={11} onClick={increaseVolume} boxHeight={boxHeight} boxWidth={boxWidth} />
+            </div>
             {/*Audios*/}
             <audio id="audio_failure" src="sounds/access-denied.mp3" autostart="false" preload="auto" />
             <audio id="audio_success" src="sounds/correct.mp3" autostart="false" preload="auto" />
-            <audio id="audio_wheel" src="sounds/spin.wav" autostart="false" preload="auto" />          
+            <audio id="audio_beep" src="sounds/beep-short.mp3" autostart="false" preload="auto" />       
             {/** Luces de correcto o incorrecto*/}
             <div className="boxlight boxlight_off" style={{ display: light === "off" ? "block" : "none", left: props.appwidth / 2 + boxWidth / 2 * 0.3, top: props.appheight / 2 - boxHeight / 2 * 0.84 }} ></div> 
             <div className="boxlight boxlight_red" style={{ display: light === "red" ? "block" : "none", left: props.appwidth / 2 + boxWidth / 2 * 0.3, top: props.appheight / 2 - boxHeight / 2 * 0.84 }} ></div> 
